6cddb8fd6e86011d3371219b2eedc7dd29ee8c57
Samiul Huque <Samiul.Huque@gmail.com>
2019-04-27 18:17:39 -0400

    created store.js and started the initial setup

---
+++ client/store.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from "redux";
+import yourReducer from "./yourReducer";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+
+const initialState = {
+  alwaysOpen: [],
+  readSoon: [],
+  readLater: []
+};
+
+function yourReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  yourReducer,
+  applyMiddleware(thunkMiddleware, createLogger())
+);
+
+export default store;
